efe2d3530cebf1711ecce5cb3344132b455bc75e
John <johnmarksjr@gmail.com>
2014-11-13 15:10:42 -0700

    Finally got the directive controller vs link functions responsibilities figured out

--- app/app.js
+++ app/app.js
@@ -6,96 +6,70 @@ angular.module('CodeMirror', ['ui.codemirror'])
             scope: true,
             templateUrl: './snippet.html',
             controller: function($scope, $element, $attrs) {
-                var lineWrapping = false,
-                    lineNumbers = true,
-                    textDecorationNoneStyle = {'text-decoration':'none'},
-                    textDecorationLineThroughStyle = {'text-decoration':'line-through'};
-
                 $scope.snippetData = {};
                 $scope.snippetData.code = localStorage['snippetCodeModel'];
 
                 $scope.isEditing = false;
+                $scope.lineWrapping = false;
+                $scope.lineNumbers = true;
 
-                $scope.scrollStrikeStyle = textDecorationNoneStyle;
-                $scope.wrapStrikeStyle = textDecorationLineThroughStyle;
-                $scope.lineNumberStrikeStyle = textDecorationNoneStyle;
-
+                // The codemirror editor options must be done in a controller
+                // (won't work in the link function)
                 $scope.codeEditorOptions = {
-                    lineWrapping : lineWrapping,
+                    lineWrapping : $scope.lineWrapping,
                     indentUnit: 4,
-                    lineNumbers: lineNumbers,
+                    lineNumbers: $scope.lineNumbers,
                     readOnly: 'nocursor',
                     mode: 'javascript'
                 };
+            },
+            link: function(scope, element, attrs, snippetCtrl) {
+                // Use this 'link' function to do any DOM or jQuery manipulation
+                // (won't work in the controller
 
-                $scope.toggleEdit = function(snippetData) {
-                    $scope.isEditing = !$scope.isEditing
-                    if ($scope.isEditing) {
-                        $scope.codeEditorOptions.readOnly = false;
-                        $('.CodeMirror').addClass('isEditing');
+                var cmElement = element.find('.CodeMirror'),
+                    cmScrollElement = element.find('.CodeMirror-scroll'),
+                    scrolling = true,
+                    textDecorationNoneStyle = {'text-decoration':'none'},
+                    textDecorationLineThroughStyle = {'text-decoration':'line-through'};
+
+                scope.refreshIt = true;
+                scope.scrollStrikeStyle = textDecorationNoneStyle;
+                scope.wrapStrikeStyle = textDecorationLineThroughStyle;
+                scope.lineNumberStrikeStyle = textDecorationNoneStyle;
+
+                scope.toggleEdit = function(snippetData) {
+                    scope.isEditing = !scope.isEditing
+                    if (scope.isEditing) {
+                        scope.codeEditorOptions.readOnly = false;
+                        cmElement.addClass('isEditing');
                     } else {
                         saveCode(snippetData.code);
                         terminateEditing();
                     }
                 };
 
-                $scope.cancelEdit = function() {
-                    if ($scope.isEditing) {
-                        $scope.isEditing = false;
-                        $scope.snippetData.code = localStorage['snippetCodeModel'];
+                scope.cancelEdit = function() {
+                    if (scope.isEditing) {
+                        scope.isEditing = false;
+                        scope.snippetData.code = localStorage['snippetCodeModel'];
                         terminateEditing();
                     }
                 };
-
-                $scope.toggleLineWrap = function() {
-                    lineWrapping = !lineWrapping;
-                    $scope.wrapStrikeStyle =
-                        lineWrapping ? textDecorationNoneStyle : textDecorationLineThroughStyle;
-                    if (lineWrapping) {
-                        $scope.codeEditorOptions.lineWrapping = true;
-                    } else {
-                        $scope.codeEditorOptions.lineWrapping = false;
-                    }
-                };
-
-                $scope.toggleLineNumbers = function() {
-                    lineNumbers = !lineNumbers;
-                    $scope.codeEditorOptions.lineNumbers = lineNumbers;
-                    $scope.lineNumberStrikeStyle =
-                        lineNumbers ? textDecorationNoneStyle : textDecorationLineThroughStyle;
-                };
-
-                function terminateEditing() {
-                    $('.CodeMirror').removeClass('isEditing');
-                    // This line sets the textarea to readonly
-                    $scope.codeEditorOptions.readOnly = 'nocursor';
-                }
-
-                function saveCode(codeModel) {
-                    localStorage['snippetCodeModel'] = codeModel;
-                }
-            },
-            link: function(scope, element, attrs, snippetCtrl) {
-                var cmScroll = element.find('.CodeMirror-scroll'),
-                    scrolling = true,
-                    textDecorationNoneStyle = {'text-decoration':'none'},
-                    textDecorationLineThroughStyle = {'text-decoration':'line-through'};
-
-                scope.refreshIt = true;
 
                 scope.toggleScroll = function() {
                     scrolling = !scrolling;
                     scope.scrollStrikeStyle =
                         scrolling ? textDecorationNoneStyle : textDecorationLineThroughStyle;
                     if (scrolling) {
-                        // Set textarea to scroll in window of max-height = 400px
-                        cmScroll.css({
+                        // Set CodeMirror scroll element to scroll in window of max-height = 400px
+                        cmScrollElement.css({
                             'overflow':'auto',
                             'max-height':'400px'
                         })
                     } else {
-                        // Set textarea to expand to code size
-                        cmScroll.css({
+                        // Set CodeMirror scroll element to expand to code size
+                        cmScrollElement.css({
                             'overflow-x':'auto',
                             'overflow-y':'hidden',
                             'height':'auto',
@@ -104,6 +78,34 @@ angular.module('CodeMirror', ['ui.codemirror'])
                     }
                     scope.refreshIt = !scope.refreshIt;
                 };
+
+                scope.toggleLineWrap = function() {
+                    scope.lineWrapping = !scope.lineWrapping;
+                    scope.wrapStrikeStyle =
+                        scope.lineWrapping ? textDecorationNoneStyle : textDecorationLineThroughStyle;
+                    if (scope.lineWrapping) {
+                        scope.codeEditorOptions.lineWrapping = true;
+                    } else {
+                        scope.codeEditorOptions.lineWrapping = false;
+                    }
+                };
+
+                scope.toggleLineNumbers = function() {
+                    scope.lineNumbers = !scope.lineNumbers;
+                    scope.codeEditorOptions.lineNumbers = scope.lineNumbers;
+                    scope.lineNumberStrikeStyle =
+                        scope.lineNumbers ? textDecorationNoneStyle : textDecorationLineThroughStyle;
+                };
+
+                function terminateEditing() {
+                    cmElement.removeClass('isEditing');
+                    // This line sets the textarea to readonly
+                    scope.codeEditorOptions.readOnly = 'nocursor';
+                }
+
+                function saveCode(codeModel) {
+                    localStorage['snippetCodeModel'] = codeModel;
+                }
             }
         }
     }]);
